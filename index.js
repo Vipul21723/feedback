3e09e447cb557a3d508f47b1dbca8d3d7be3f0c5
Vipul Patel <vipul21723@gmail.com>
2026-02-12 10:54:29 +0000

    New Neon

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+
+app.post("/api/feedback", async (req,res) => {
+    const { name, message } = req.body;
+    
+    try {
+        const result = await pool.query(
+            "INSERT INTO feedbacks (name, message) VALUES ($1,$2) RETURNING *", [name,message]
+        );
+
+        res.json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}) 
+
+
+app.get("/api/feedbacks", async (req,res) => {
+    try {
+        const result = await pool.query(
+            "SELECT * FROM feedbacks ORDER BY createdat DESC"
+        );
+        res.json(result.rows);
+    } catch (err) {
+        res.status(500).json({ error: err.message})
+    }
+})
+
+app.listen(3000, () => {
+    console.log("Server Started ... !!!");
+})
