fde87ff266f51155ab7f093ab59b23af1620b7ff
Vipul Patel <vipul21723@gmail.com>
2026-02-12 16:47:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const { Pool } = require("pg");
 const app = express();
 app.use(cors())
 app.use(express.json());
-
+const pool = require("./db");
 
 app.post("/api/feedback", async (req,res) => {
     const { name, message } = req.body;
     
     try {
         const result = await pool.query(
-            "INSERT INTO feedbacks (name, message) VALUES ($1,$2) RETURNING *", [name,message]
+            `INSERT INTO public.feedbacks (name, message) VALUES ($1,$2) RETURNING *`, [name,message]
         );
 
         res.json(result.rows[0]);
@@ -25,7 +25,7 @@ app.post("/api/feedback", async (req,res) => {
 app.get("/api/feedbacks", async (req,res) => {
     try {
         const result = await pool.query(
-            "SELECT * FROM feedbacks ORDER BY createdat DESC"
+            `SELECT * FROM public.feedbacks ORDER BY createdat DESC`
         );
         res.json(result.rows);
     } catch (err) {
